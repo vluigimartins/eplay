62302b498e3625d71641af959c91d4cc904d00b6
Vinicius <vini-luiz@hotmail.com>
2023-11-02 11:08:39 -0300

    Finalizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+
 import Rotas from './routes'
 import Footer from './components/Footer'
 import { store } from './store'
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
-import { cores } from '../../styles'
+import { breakpoints, colors } from '../../styles'
 
-type InnputGroupProps = {
+type InputGroupProps = {
   maxWidth?: string
 }
 
@@ -18,9 +18,13 @@ export const Row = styled.div<RowProps>`
   column-gap: 24px;
   margin-top: ${(props) => props.marginTop || '0'};
   align-items: flex-end;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    display: block;
+  }
 `
 
-export const InputGroup = styled.div<InnputGroupProps>`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
 
   max-width: ${(props) => props.maxWidth || 'auto'};
@@ -33,19 +37,29 @@ export const InputGroup = styled.div<InnputGroupProps>`
 
   input,
   select {
-    background-color: ${cores.branco};
-    border: 1px solid ${cores.branco};
+    background-color: ${colors.white};
+    border: 1px solid ${colors.white};
     height: 32px;
     padding: 0 8px;
     width: 100%;
+
+    &.error {
+      border: 1px solid red;
+    }
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-top: 16px;
   }
 `
+
 export const TabButton = styled.button<TabButtonProps>`
   border-radius: 8px;
   font-size: 14px;
   font-weight: bold;
-  color: ${cores.branco};
-  background-color: ${(props) => (props.isActive ? cores.verde : cores.preto)};
+  color: ${colors.white};
+  background-color: ${(props) =>
+    props.isActive ? colors.green : colors.black};
   height: 32px;
   border: none;
   margin-right: 16px;
@@ -54,5 +68,10 @@ export const TabButton = styled.button<TabButtonProps>`
 
   img {
     margin-right: 8px;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-top: 8px;
+    width: 100%;
   }
 `
